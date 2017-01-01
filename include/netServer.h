548805f3513a4ac875096efecd7d19488c16b126
Nathan King <nking1212@outlook.com>
2017-01-01 04:31:28 -0500

    Licensing Junk

--- include/netServer.h
+++ include/netServer.h
@@ -1,3 +1,19 @@
+/*
+Copyright (C) 2017  Nathan King
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef NETSERVER_H
 #define NETSERVER_H
 #define _WIN32_WINNT 0x501
